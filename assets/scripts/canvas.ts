5a43183d065fa69936355e87246708a41b5fb3b7
RohitSM7882 <rohit.maranal@gmail.com>
2021-03-21 22:48:36 +0530

    Update canvas.ts

--- assets/scripts/canvas.ts
+++ assets/scripts/canvas.ts
@@ -13,27 +13,23 @@ export default class NewClass extends cc.Component {
     @property(cc.Node)
     instructionTop: cc.Node = null;
 
+    // @property(cc.Label)
+    // instruction: cc.Label  = null;
+
     @property(cc.Node)
     baseSlider: cc.Node = null;
 
     onLoad () {
-        // this.instructionTop.runAction(cc.moveTo(1.0,cc.v2(0,180)));
-        // cc.delayTime(3.0);
-        // this.baseSlider.runAction(cc.moveTo(1.0, cc.v2(0,-180)));
         this.instructionTop.opacity = 0;
         this.baseSlider.opacity = 0;
         var count = 0;
         setInterval(()=>{
             count += 1;
             if(count == 1){
-                var sequence = cc.sequence(cc.fadeOut(0),cc.fadeIn(1.0));
-                this.instructionTop.runAction(sequence);
-                this.instructionTop.runAction(cc.moveTo(1.0,cc.v2(0,180)));
+                this.node.emit('moveNode','instructionTop');
             }
             else if(count == 2){
-                var sequence = cc.sequence(cc.fadeOut(0),cc.fadeIn(1));
-                this.baseSlider.runAction(sequence);
-                this.baseSlider.runAction(cc.moveTo(1.0, cc.v2(0,-180)));
+                this.node.emit('moveNode','baseSlider');
             }
                 
         },1000);
